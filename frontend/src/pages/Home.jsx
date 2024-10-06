9834f584e3db30eedeedaf3428794058d063eb24
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-10-05 18:55:37 -0600

    temporary note testing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,47 @@
+import {useState, useEffect} from 'react'
+import api from '../api'
+
 function Home() {
+    const [notes, setNotes] = useState([])
+    const [content,setContent] = useState("")
+    const [title, setTitle] = useState("")
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    const getNotes = () => {
+        api.get("/api/notes/").then((res) => res.data).then((data) => setNotes(data)).catch((err) => alert(err))
+    }
+
+    const deleteNote = (id) => {
+        api.delete(`api/notes/delete/${id}/`).then((res) => {
+            if (res.status === 204) alert("Deleted!")
+            else alert("Failed to delete!")
+        }).catch((err) => alert(err))
+        getNotes()
+    }
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api.post("/api/notes/", {content,title}).then((res) => {
+            if (res.status === 201) alert("Created!")
+            else alert("Failed")
+        }).catch((err) => alert(err))
+        getNotes()
+    }
 
     return (
         <>
-        
+        <div>
+            <h2>Notes</h2>
+        </div>
+        <h2>Create Note</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+        <br />
+        <input type="text" id="title" name="title" required onChange={(e) => setTitle(e.target.value)} value={title}/>
+        </form>
         </>
     )
 
